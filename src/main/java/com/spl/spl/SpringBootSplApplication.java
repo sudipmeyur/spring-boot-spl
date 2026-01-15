be6289c663c52ab32e046ef56259fee929b63d0a
Sudipmeyur <meyur.sudip@gmail.com>
2026-01-16 00:43:08 +0530

    Created At Updated at

--- src/main/java/com/spl/spl/SpringBootSplApplication.java
+++ src/main/java/com/spl/spl/SpringBootSplApplication.java
@@ -2,8 +2,10 @@ package com.spl.spl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringBootSplApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/spl/spl/entity/PlayerTeam.java
+++ src/main/java/com/spl/spl/entity/PlayerTeam.java
@@ -1,7 +1,12 @@
 package com.spl.spl.entity;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
@@ -12,6 +17,7 @@ import lombok.Data;
 @Entity
 @Table(name = "player_team")
 @IdClass(PlayerTeamId.class)
+@EntityListeners(AuditingEntityListener.class)
 @Data
 public class PlayerTeam {
 
@@ -31,4 +37,10 @@ public class PlayerTeam {
 	private Season season;
 
 	private BigDecimal soldAmount;
+
+	@CreatedDate
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	private LocalDateTime updatedAt;
 }
